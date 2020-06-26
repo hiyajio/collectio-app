ceb7bcae77b2245c6bb39ac4881de955b2633a0b
lockjio <jio.buenviaje@gmail.com>
2020-06-26 02:55:04 -0400

    Initializa header and implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
+import Header from "./components/header/header.component.jsx";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
 	return (
 		<div>
+			{/* By placing Header above Switch, this ensure it is only rendered once
+			and will stay regardless of which page is chosen*/}
+			<Header />
 			{/* Switch is its namesake. Only the first one we find that matches path
 			will be rendered. Think of it as a switch case or an if statement with
 			continue*/}
