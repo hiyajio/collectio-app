63bf3bef345a12e5c45fa706407a4861b63c664a
lockjio <jio.buenviaje@gmail.com>
2020-06-26 07:57:14 -0400

    Complete user database search look up and create user functions

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import HomePage from "./pages/homepage/homepage.page";
 import ShopPage from "./pages/shop/shop.page";
 import SignInSignUpPage from "./pages/sign-in-sign-up/sign-in-sign-up.page";
 
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 // Need to turn into Component since need to keep track of state for OAuth
 class App extends Component {
@@ -32,9 +32,28 @@ class App extends Component {
 	loads after getting called) */
 	componentDidMount() {
 		/* Used for user persistence. Once signed in, if they did not sign out,
-		next visit they will still be signed in */
-		this.unsubcribeFromAuth = auth.onAuthStateChanged((user) => {
-			this.setState({ currentUser: user });
+		next visit they will still be signed in. Also store user in database. */
+		this.unsubcribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+			// If successfully retrieved or created user in database
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+
+				/* Backend to frontend transfer: Using the current state or
+				"Snapshot" of that user, we simple set the state of
+				currentUser to it. */
+				userRef.onSnapshot((snapShot) => {
+					this.setState({
+						currentUser: {
+							// Store user id, displayName and all other info
+							id: snapShot.id,
+							...snapShot.data(),
+						},
+					});
+				});
+			} else {
+				// Checks for no user and sets it to null i.e. no one signed in
+				this.setState({ currentUser: userAuth });
+			}
 		});
 	}
 
@@ -48,11 +67,11 @@ class App extends Component {
 		return (
 			<div>
 				{/* By placing Header above Switch, this ensure it is only rendered once
-			and will stay regardless of which page is chosen*/}
+				and will stay regardless of which page is chosen*/}
 				<Header currentUser={this.state.currentUser} />
 				{/* Switch is its namesake. Only the first one we find that matches path
-			will be rendered. Think of it as a switch case or an if statement with
-			continue*/}
+				will be rendered. Think of it as a switch case or an if statement with
+				continue*/}
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -16,6 +16,41 @@ const config = {
 // Initialize firebase instance with custom config given
 firebase.initializeApp(config);
 
+// Set up data received from App to be added to Firebase Firestore (database)
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	// If user ID does not exist in Firestore i.e. junk data or null, return
+	if (!userAuth) return;
+
+	// If user ID does exist in database, retrieve the user ID
+	const userRef = firestore.doc(`user/${userAuth.uid}`);
+
+	// Get the snapshot or current state of that user from database for further use
+	const snapShot = await userRef.get();
+
+	// If user is not yet on database, add them to database
+	if (!snapShot.exists) {
+		// displayName & email already existing properties in userAuth
+		const { displayName, email } = userAuth;
+		// Log current time in order to store time of user creation in database
+		const createdAt = new Date();
+
+		// try-catch block since using await function
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData,
+			});
+		} catch (error) {
+			console.log("error creating user", error.message);
+		}
+	}
+
+	// Returned since we need user info for other functions throughout app
+	return userRef;
+};
+
 // Export for auth and firestore access
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
