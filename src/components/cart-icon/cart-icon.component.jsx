4cf6895f269815f94d112e7577b7bdee4579683a
lockjio <jio.buenviaje@gmail.com>
2020-07-02 12:32:15 -0400

    Add display total number of items in cart function

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -10,14 +10,25 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import "./cart-icon.styles.scss";
 
 // Destructuring 'prop' into their specific counterpart for syntactic sugar
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
 	// On click, toggle showing of Cart Dropdown
 	<div className="cart-icon" onClick={toggleCartHidden}>
 		<ShoppingIcon className="shopping-icon" />
 		{/* Counter within Icon for items in cart */}
-		<span className="item-count">0</span>
+		<span className="item-count">{itemCount}</span>
 	</div>
 );
+
+// Gain access to cartItems state
+// Nested destructuring for more syntactic sugar
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+	// Calculate the total number of items in the cart to display as number
+	itemCount: cartItems.reduce(
+		(accumulatedQuantity, cartItem) =>
+			accumulatedQuantity + cartItem.quantity,
+		0
+	),
+});
 
 // Update and dispatch global redux reducer to all listeners
 const mapDispatchToProps = (dispatch) => ({
@@ -25,4 +36,4 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 // Pass it again since one-way data flow
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
