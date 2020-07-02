7929b4c8ba659126f0b9b01e6638d1e398c5ba06
lockjio <jio.buenviaje@gmail.com>
2020-07-02 08:22:56 -0400

    Create cart item preview and connect to dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 
+// Needed for redux state management
+import { connect } from "react-redux";
+
 import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
 
 import "./cart-dropdown.styles.scss";
 
-const CartDropdown = () => (
+// Destructuring 'prop' into their specific counterpart for syntactic sugar
+const CartDropdown = ({ cartItems }) => (
 	<div className="cart-dropdown">
-		<div className="cart-items" />
+		{/* Map through all current items in cart */}
+		<div className="cart-items">
+			{cartItems.map((cartItem) => (
+				<CartItem key={cartItem.id} item={cartItem} />
+			))}
+		</div>
 		<CustomButton>CHECKOUT</CustomButton>
 	</div>
 );
 
-export default CartDropdown;
+// Gain access to cartItems state
+// Nested destructuring for more syntactic sugar
+const mapStateToProps = ({ cart: { cartItems } }) => ({ cartItems });
+
+// Pass it again since one-way data flow
+export default connect(mapStateToProps)(CartDropdown);
