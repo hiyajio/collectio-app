43fea36dfc2106d08556935eb7f2f3a9ea9d68c6
lockjio <jio.buenviaje@gmail.com>
2020-07-02 19:54:23 -0400

    Add checkout item increment/decrement arrows

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -3,12 +3,16 @@ import React from "react";
 // Needed for redux state management
 import { connect } from "react-redux";
 
-import { clearItemFromCart } from "../../redux/cart/cart.actions";
+import {
+	clearItemFromCart,
+	addItem,
+	removeItem,
+} from "../../redux/cart/cart.actions";
 
 import "./checkout-item.styles.scss";
 
 // Destructuring 'prop' into their specific counterpart for syntactic sugar
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
 	// Further destructuring to gain access to specific props
 	const { name, imageUrl, price, quantity } = cartItem;
 	return (
@@ -17,10 +21,20 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 				<img src={imageUrl} alt="item" />
 			</div>
 			<span className="name">{name}</span>
-			<span className="quantity">{quantity}</span>
+			<span className="quantity">
+				{/* Remove item by 1 from cart when button is clicked */}
+				<div className="arrow" onClick={() => removeItem(cartItem)}>
+					&#10094;
+				</div>
+				<span className="value">{quantity}</span>
+				{/* Add item by 1 from cart when button is clicked */}
+				<div className="arrow" onClick={() => addItem(cartItem)}>
+					&#10095;
+				</div>
+			</span>
 			<span className="price">{price}</span>
 			{/* UTF-8 Wingdings equivalent for an 'X' mark */}
-			{/* Remove item from cart when button is clicked */}
+			{/* Remove entire unique item from cart when button is clicked */}
 			<div className="remove-button" onClick={() => clearItem(cartItem)}>
 				&#10005;
 			</div>
@@ -31,6 +45,8 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 // Update and dispatch global redux reducer to all listeners
 const mapDispatchToProps = (dispatch) => ({
 	clearItem: (item) => dispatch(clearItemFromCart(item)),
+	addItem: (item) => dispatch(addItem(item)),
+	removeItem: (item) => dispatch(removeItem(item)),
 });
 
 // Pass it again since one-way data flow
