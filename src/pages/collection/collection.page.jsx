8c03fd1627ae9c52054eff5dbbb51efcd337d208
lockjio <jio.buenviaje@gmail.com>
2020-07-03 02:57:51 -0400

    Add comments

--- src/pages/collection/collection.page.jsx
+++ src/pages/collection/collection.page.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Needed for redux state management
 import { connect } from "react-redux";
 
 import CollectionItem from "../../components/collection-item/collection-item.component";
@@ -7,12 +8,16 @@ import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.styles.scss";
 
+// Destructuring 'prop' into their specific counterpart for syntactic sugar
 const CollectionPage = ({ collection }) => {
+	// Further destructuring to gain access to specific props
 	const { title, items } = collection;
 	return (
 		<div className="collection-page">
 			<h2 className="title">{title}</h2>
 			<div className="items">
+				{/* When mapping, a key is needed to tell React (ReactDOM)
+				which component is which */}
 				{items.map((item) => (
 					<CollectionItem key={item.id} item={item} />
 				))}
@@ -27,4 +32,5 @@ const mapStateToProps = (state, ownProps) => ({
 	collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
 
+// Pass it again since one-way data flow
 export default connect(mapStateToProps)(CollectionPage);
