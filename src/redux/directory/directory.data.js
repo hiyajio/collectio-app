9c3e14996de2a5a9c05c537f0208033e04ef93b9
lockjio <jio.buenviaje@gmail.com>
2020-07-04 03:44:51 -0400

    Update database schema to add pointers & refs

--- src/redux/directory/directory.data.js
+++ src/redux/directory/directory.data.js
@@ -4,12 +4,16 @@ const DIRECTORY_DATA = [
 		title: "premium items",
 		imageUrl: "https://i.ibb.co/hBRGwxH/nm-98-f.jpg",
 		id: 1,
+		directoryID: 101, // Add unique identifier to serve as pointer reference
+		directory: "premium-items",
 		linkUrl: "categories/premium",
 	},
 	{
 		title: "regular items",
 		imageUrl: "https://i.ibb.co/nbXgQ0h/xmewa-12.jpg",
 		id: 2,
+		directoryID: 102, // Add unique identifier to serve as pointer reference
+		directory: "regular-items",
 		linkUrl: "categories/regular",
 	},
 ];
