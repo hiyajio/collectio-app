a9d65c0859f81a0814360e5f7f443d74c0e64e49
lockjio <jio.buenviaje@gmail.com>
2020-07-20 20:38:24 -0400

    Instantiate root saga

---
+++ src/redux/root-saga.js
@@ -0,0 +1,13 @@
+import { all, call } from "redux-saga/effects";
+
+import { fetchCollectionsStart } from "./shop/shop.sagas";
+
+// Same as root-reducer, we need to group all sagas and fire them at the same time
+export default function* rootSaga() {
+	/* all effect simply allows us to run multiple sagas at the same time since
+    if we don't use it, we have to have multiple yields which won't allow us
+    to run those sagas concurrently (yield == await) */
+	/* call effect is essentially a function call. Syntax below is equivalent to
+    fetchCollectionsStart() => Still works, this is just the saga way of doing things */
+	yield all([call(fetchCollectionsStart)]);
+}
